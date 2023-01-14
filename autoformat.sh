d4e769a2ae024bdba69ffa7aaff88bd8a1f714f0
Jack Myers <jackhmyers97@gmail.com>
2023-01-13 18:02:42 -0800

    added cmake-format

--- autoformat.sh
+++ autoformat.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 find . -regex './include/.*\.\(cuh\|cu\)' -exec clang-format -style=file -i {} \;
 find . -regex './tests/.*\.\(cuh\|cu\)' -exec clang-format -style=file -i {} \;
+cmake-format CMakeLists.txt -o CMakeLists.txt
+cmake-format tests/CMakeLists.txt -o tests/CMakeLists.txt
