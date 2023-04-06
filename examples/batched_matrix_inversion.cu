e7c68bf10c4a952e49fd2c699020ab36a28f7e82
Jack Myers <jackhmyers97@gmail.com>
2023-04-05 21:39:42 -0700

    implemented batched matrix inversion

--- examples/batched_matrix_inversion.cu
+++ examples/batched_matrix_inversion.cu
@@ -30,5 +30,95 @@
 // POSSIBILITY OF SUCH DAMAGE.
 
 #include <naga/linalg/batched_matrix_inverse.cuh>
+#include <scalix/algorithm/reduce.cuh>
+#include <chrono>
 
+int main() {
+    // clang-format off
+    float A_inv_check[3][3] = {
+        {
+            -1.791666667, 0.916666667, -0.125000000,
+        },
+        {
+            1.583333333, -0.833333333, 0.250000000,
+        },
+        {
+            -0.125000000, 0.250000000, -0.125000000,
+        },
+    };
 
+    float A[3][3] = {
+        {
+            1.0, 2.0, 3.0,
+        },
+        {
+            4.0, 5.0, 6.0,
+        },
+        {
+            7.0, 8.0, 1.,
+        },
+    };
+    // clang-format on
+
+    size_t batch_size = 10'000'000;
+    sclx::array<float, 3> A_batched{3, 3, batch_size};
+
+    sclx::execute_kernel([&](sclx::kernel_handler& handler) {
+        handler.launch(
+            sclx::md_range_t<3>(A_batched.shape()),
+            A_batched,
+            [=] __device__(const sclx::md_index_t<3>& index, const auto&) {
+                A_batched[index] = A[index[0]][index[1]];
+            }
+        );
+    }).get();
+
+    auto begin = std::chrono::high_resolution_clock::now();
+    auto A_inv_batched = naga::linalg::batched_matrix_inverse(A_batched);
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed = end - begin;
+    std::cout << "Elapsed time: " << elapsed.count() * 1000 << " ms"
+              << std::endl;
+
+    auto error_count = sclx::zeros<int>({batch_size});
+
+    sclx::execute_kernel([&](sclx::kernel_handler& handler) {
+        handler.launch(
+            sclx::md_range_t<1>(error_count.shape()),
+            error_count,
+            [=] __device__(const sclx::md_index_t<1>& index, const auto&) {
+                float tol = 1e-6;
+                for (int i = 0; i < 3; ++i) {
+                    for (int j = 0; j < 3; ++j) {
+                        if (std::abs(
+                                A_inv_batched(i, j, index[0])
+                                - A_inv_check[i][j]
+                            )
+                            > tol) {
+                            error_count[index] += 1;
+                            return;
+                        }
+                    }
+                }
+            }
+        );
+    }).get();
+
+    auto sum_errors
+        = sclx::algorithm::reduce(error_count, 0, sclx::algorithm::plus<>());
+
+    if (sum_errors != 0) {
+        std::cerr << "Error: " << sum_errors << " errors found." << std::endl;
+        return 1;
+    }
+
+    auto first_A_inv = A_inv_batched.get_slice({0});
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            std::cout << first_A_inv(i, j) << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
--- naga/linalg/batched_matrix_inverse.cuh
+++ naga/linalg/batched_matrix_inverse.cuh
@@ -31,9 +31,119 @@
 // POSSIBILITY OF SUCH DAMAGE.
 
 #pragma once
+#include <scalix/fill.cuh>
+#include "detail/batched_matrix_inverse.cuh"
+#include <future>
 
 namespace naga::linalg {
 
 template<class T>
+__host__ sclx::array<T, 3> batched_matrix_inverse(sclx::array<T, 3>& A) {
 
+    if (A.shape()[0] != A.shape()[1]) {
+        sclx::throw_exception<std::invalid_argument>(
+            "Matrices must be square",
+            "naga::linalg::"
+        );
+    }
+
+    if (A.shape()[2] >= std::numeric_limits<int>::max()) {
+        sclx::throw_exception<std::invalid_argument>(
+            "Batch size must be less than std::numeric_limits<int>::max()",
+            "naga::linalg::"
+        );
+    }
+
+    sclx::array<T, 3> A_inv = sclx::zeros<T>({A.shape()[0], A.shape()[1], A.shape()[2]});
+    const std::vector<std::tuple<int, size_t, size_t>>& split_info
+        = sclx::get_device_split_info(A);
+    A_inv.set_primary_devices(split_info);
+
+    auto current = std::experimental::source_location::current();
+
+    std::vector<std::future<void>> futures;
+
+    for (const auto& [device_id, slice_start, slice_len] : split_info) {
+        auto A_slice = A.get_range({slice_start}, {slice_len});
+        auto A_inv_slice = A_inv.get_range({slice_start}, {slice_len});
+
+        sclx::array<T*, 1> A_slice_ptr = sclx::zeros<T*>({A_slice.shape()[2]});
+        sclx::array<T*, 1> A_inv_slice_ptr = sclx::zeros<T*>({A_inv_slice.shape()[2]});
+        auto ptr_setup_fut = sclx::execute_kernel([&] (sclx::kernel_handler& handler) {
+            sclx::array_list<T*, 1, 2> result = {A_slice_ptr, A_inv_slice_ptr};
+            handler.launch(
+                sclx::md_range_t<1>(A_slice_ptr.shape()),
+                result,
+                [=] __device__ (sclx::md_index_t<1> &idx, auto &) {
+                    result[0][idx] = &A_slice(0, 0, idx[0]);
+                    result[1][idx] = &A_inv_slice(0, 0, idx[0]);
+                }
+            );
+        });
+
+        auto lambda = [=] (int d_id, std::future<void> ptr_setup_fut) {
+            sclx::cuda::set_device(d_id);
+
+            cublasHandle_t handle;
+            cublasCreate(&handle);
+            int *info, *pivot;
+            int dims = A_slice.shape()[0];
+            int batch_size = A_slice.shape()[2];
+            cudaMalloc(&info, batch_size * sizeof(int*));
+            cudaMalloc(&pivot, batch_size * dims * sizeof(int*));
+
+            ptr_setup_fut.get();
+            auto status =detail::cublas_getrf_batched<T>::compute(
+                handle,
+                dims,
+                A_slice_ptr.data().get(),
+                dims,
+                pivot,
+                info,
+                batch_size
+            );
+
+            if (status != CUBLAS_STATUS_SUCCESS) {
+                sclx::throw_exception<std::runtime_error>(
+                    "CUBLAS error: " + std::to_string(status),
+                    "naga::linalg::",
+                    current
+                );
+            }
+
+            status = detail::cublas_getri_batched<T>::compute(
+                handle,
+                dims,
+                A_slice_ptr.data().get(),
+                dims,
+                pivot,
+                A_inv_slice_ptr.data().get(),
+                dims,
+                info,
+                batch_size
+            );
+
+            if (status != CUBLAS_STATUS_SUCCESS) {
+                sclx::throw_exception<std::runtime_error>(
+                    "CUBLAS error: " + std::to_string(status),
+                    "naga::linalg::",
+                    current
+                );
+            }
+
+            cudaFree(info);
+            cudaFree(pivot);
+            cublasDestroy(handle);
+        };
+
+        futures.push_back(std::async(std::launch::async, lambda, device_id, std::move(ptr_setup_fut)));
+    }
+
+    for (auto& future : futures) {
+        future.get();
+    }
+
+    return A_inv;
 }
+
+}  // namespace naga::linalg
